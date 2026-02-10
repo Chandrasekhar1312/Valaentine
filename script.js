e9d3eb11a99553baba79389d2f9ce53e3fcf69e8
Chandrasekhar1312 <111875920+Chandrasekhar1312@users.noreply.github.com>
2026-02-10 08:20:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const yesBtn = document.getElementById("yesBtn");
+const noBtn = document.getElementById("noBtn");
+
+yesBtn.addEventListener("click", () => {
+  alert("Yay! 💖 Happy Valentine’s Day!");
+});
+
+noBtn.addEventListener("click", () => {
+  alert("Oh no 😢 Still friends!");
+});
